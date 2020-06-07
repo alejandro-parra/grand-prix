cc5f9bf480c499cabc6c141c7ce464d987921f1d
Alejandro Parra <alexparra_07@hotmail.com>
2020-06-07 12:16:45 -0500

    create repo

---
+++ grand-prix.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"image/png"
+	"io"
+	"os"
+)
+
+var grid [][]bool
+
+func main() {
+	//initialize the grid which will be the playable space
+	grid = make([][]bool, 50)
+	for i := range grid {
+		grid[i] = make([]bool, 100)
+	}
+	//load the image of the course and read the pixels to make the grid
+	gridImageFile, err := os.Open("grid.png")
+	if err != nil {
+		println("error reading image")
+		os.Exit(1)
+	}
+
+	setupGrid(gridImageFile)
+
+	/*for i := range grid {
+		for j := range grid[i] {
+			grid[i][j] = false
+			print(grid[i][j])
+		}
+		println("")
+	}*/
+
+}
+
+// Get the bi-dimensional pixel array
+func setupGrid(gridImageFile io.Reader) {
+	gridImage, err := png.Decode(gridImageFile)
+	if err != nil {
+		println("error decoding image")
+		os.Exit(2)
+	}
+
+	bounds := gridImage.Bounds()
+	width, height := bounds.Max.X, bounds.Max.Y
+
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			r, g, b, a := gridImage.At(x, y).RGBA()
+			_ = a
+			if r == 0 && g == 0 && b == 0 {
+				grid[y][x] = true
+				print("X")
+			} else {
+				grid[y][x] = false
+				print(" ")
+			}
+
+		}
+		println("")
+	}
+}
